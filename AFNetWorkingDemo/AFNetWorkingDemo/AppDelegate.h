05da7d15acbe5f6337eb813c49d3ec35229614e0
dragon <dragon@dragon.lan>
2016-04-17 14:48:37 +0800

    creat project

---
+++ AFNetWorkingDemo/AFNetWorkingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetWorkingDemo
+//
+//  Created by dragon on 16/4/17.
+//  Copyright © 2016年 dragon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetWorkingDemo/AFNetWorkingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetWorkingDemo
+//
+//  Created by dragon on 16/4/17.
+//  Copyright © 2016年 dragon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
